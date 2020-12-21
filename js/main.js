297fd3b211222c1f6e2eafafe017eb81d22586c9
Stuart <stu-b-doo@users.noreply.github.com>
2020-12-21 19:00:08 +0000

    parse url params

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+// install sw
 window.onload = () => {
   'use strict';
-
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js');
   }
 }
+
+// parse url params for share target
+window.addEventListener('DOMContentLoaded', () => {
+  const parsedUrl = new URL(window.location);
+  document.getElementById('title').textContent = parsedUrl.searchParams.get('title');
+  document.getElementById('text').textContent = parsedUrl.searchParams.get('text');
+  document.getElementById('url').textContent = parsedUrl.searchParams.get('url');
+});
